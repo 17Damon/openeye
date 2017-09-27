d4a4fcc22d685594c9331c33c1ea3d5382a20573
miss61008596 <61008596@qq.com>
2017-09-27 14:36:08 +0800

    edit

--- production/schema/resolvers/vedio/getVedioList.js
+++ production/schema/resolvers/vedio/getVedioList.js
@@ -26,7 +26,7 @@ async function getVedioList() {
             } else {
                 var totalCount = obj[0].totalCount;
                 var vedioList = obj[0].vedioList;
-                var endCursor = (arguments[1].offset + arguments[1].count < obj[0].totalCount ? arguments[1].offset + arguments[1].count : obj[0].totalCount) - 1;
+                var endCursor = arguments[1].offset + arguments[1].count < obj[0].totalCount ? arguments[1].offset + arguments[1].count : obj[0].totalCount;
                 var hasNextPage = endCursor < obj[0].totalCount ? true : false;
                 var pageInfo = new _objects.PageInfo(endCursor, hasNextPage);
                 var vedioListTemp = new _objects.VedioList(totalCount, vedioList, pageInfo);
